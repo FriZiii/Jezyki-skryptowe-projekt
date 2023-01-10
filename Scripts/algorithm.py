8bd92883a754e466d8ac1387cf0f2948dec6482f
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2023-01-10 19:41:31 +0100

    Fixes

--- Scripts/algorithm.py
+++ Scripts/algorithm.py
@@ -1,5 +1,3 @@
-import time
-
 def check_prime(n):
     for i in range(2, n):
         if (n%i) ==0:
@@ -35,20 +33,25 @@ def max_num(result):
             max = len(i)
     return max
 
-#file = open('E:/Studia/Jezyki-skryptowe/Projekt/Jezyki-skryptowe-projekt/results.txt', 'w')
+file_input = open("Scripts/INPUT/input.txt", "r")
+input = file_input.readline()
+n = int(input)
+file_input.close()
 
-n = int(input("║ Jaką liczbę chcesz otrzymać poprzez sumowanie liczb pierwszych: "))
+file_output = open("Scripts/OUTPUT/output.txt", "w")
 primes = get_primes(n)
-
+print(primes)
 result = sums_up_to(n)
 count = len(result)
 max = max_num(result)
 
 table = []
 for i in range(2, max+1):
-    #file.write(str('-'*16+ f"Suma {str(i)} liczb dajaca wynik {str(n)}" +'-'*16 +"\n"))
+    file_output.write(str('-'*16+ f"Suma {str(i)} liczb dajaca wynik {str(n)}" +'-'*16 +"\n"))
     table.append([])
     for r in result:
         if len(r) == i:
-            #file.write(str(r) + "\n")   
+            file_output.write(str(r) + "\n")   
             table[i-2].append(r)
+
+file_output.close()
--- Scripts/raport.py
+++ Scripts/raport.py
@@ -1,7 +1,10 @@
-﻿from tabulate import tabulate
-from datetime import datetime
+﻿from datetime import datetime
+
 import algorithm
 from graph import plt
+
+import os
+import webbrowser
 
 plt.savefig("Raport/plot.png", transparent=True)
 
@@ -10,15 +13,22 @@ file = open(path, 'w',  encoding="utf-8")
 html =""
 
 now = datetime.now()
-now = now.strftime("%d/%m/%Y %H:%M:%S")
+now_file = now
+now_web = now
+now_file = now_file.strftime("%d_%m_%Y_%H.%M.%S")
+now_web = now_web.strftime("%d/%m/%Y/%H:%M:%S")
+date = open("Raport/date.txt", "w")
+date.write(now_file)
+date.close()
 
-html += f"<html lang=\"pl\"><head><meta charset=\"utf-8\"><title>Raport z dnia {now}</title><link rel=\"stylesheet\" href=\"style.css\"></head><body>"
+
+html += f"<html lang=\"pl\"><head><meta charset=\"utf-8\"><title>Raport z dnia {now_web}</title><link rel=\"stylesheet\" href=\"style.css\"></head><body>"
 
 html +="<div class = \"container\">"
 html +="<div class = \"child\"><div class = \"raport\"><img src =\"plot.png\", alt=\"graph\"></div></div>"
 
 html +=(f"<div class = \"child\">")
-html +=(f"<div class = \"raport\"><h1>Raport z dnia {now}</h1></div>")
+html +=(f"<div class = \"raport\"><h1>Raport z dnia {now_web}</h1></div>")
 html += (f"<div class = \"data\"><h2>Wartość wejsciowa</h2><h2 class = \"text-data\">{algorithm.n}</h2>")
 html += (f"<h2>Rozpatrywane liczby pierwsze: </h2><h2 class = \"text-data\">{algorithm.primes}</h2>")
 html += (f"<h2>Ilość kombinacji licz pierwszych, których suma daje wynik {algorithm.n} </h2><h2 class = \"text-data\"> {algorithm.count}</h2>")
@@ -42,3 +52,7 @@ html += "</div>"
 html+=("</body></html>")
 file.write(html)
 file.close()
+
+path = (os.path.abspath(__file__))
+path = path.strip('\\Scripts\\raport.py') + 't\\Raport\\raport.html'
+webbrowser.open_new_tab(path)
